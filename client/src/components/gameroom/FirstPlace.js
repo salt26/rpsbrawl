433f14947ff91c23bacadf46fe3488b01f711f60
flowersayo <flowersayo0811@ewhain.net>
2022-12-31 21:11:05 +0900

    feat: Update FirstPlace

--- client/src/components/gameroom/FirstPlace.js
+++ client/src/components/gameroom/FirstPlace.js
@@ -4,21 +4,21 @@ import { Medium } from "../../styles/font";
 import styled from "styled-components";
 import BgBox from "../common/BgBox";
 import SvgIcon from "../common/SvgIcon";
+
 //1등 점수 정보
-export default function FirstPlace({}) {
-  const belong = "KING";
-  const name = "김서연";
-  const score = 7;
+export default function FirstPlace({ place }) {
+  const { affiliation, score, name } = place;
+
   return (
     <BgBox width={"350px"} height={"100px"}>
       <Row>
         <SvgIcon src={TrophySrc} size={"80px"} />
         <Col>
-          <Medium size="35px">{belong}</Medium>
+          <Medium size="35px">{affiliation}</Medium>
 
           <Medium size="25px">{name}</Medium>
         </Col>
-        <Medium size="40px">{score > 0 ? "+" + score : "-" + score}</Medium>
+        <Medium size="40px">{score >= 0 ? "+" + score : score}</Medium>
       </Row>
     </BgBox>
   );
