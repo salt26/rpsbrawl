ab9ed5274e388173f551ed04c144bd795f3a6455
flowersayo <flowersayo0811@ewhain.net>
2022-12-31 21:52:07 +0900

    feat: Show Result

--- client/src/pages/GameResultPage.js
+++ client/src/pages/GameResultPage.js
@@ -2,12 +2,24 @@ import React from "react";
 import ResultBoard from "../components/gameroom/ResultBoard";
 import Button from "../components/common/Button";
 import styled from "styled-components";
+import HTTP from "../utils/HTTP";
+import { useParams } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { getUserId } from "../utils/User";
 
 export default function GameResultPage() {
+  const { room_id } = useParams();
+  const { state } = useLocation(); // 손 목록 정보, 게임 전적 정보
+  const person_id = getUserId();
+  const navigate = useNavigate();
+
+  const _quitGame = () => {
+    navigate("/");
+  };
   return (
     <Row>
-      <Button text="나가기" />
-      <ResultBoard />
+      <Button text="나가기" onClick={_quitGame} />
+      <ResultBoard result={state} />
       <Button text="결과저장" />
     </Row>
   );
