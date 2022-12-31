cb869786c5edbc67fb7f75693f83643228211c57
flowersayo <flowersayo0811@ewhain.net>
2022-12-31 16:07:21 +0900

    feat: start a Game

--- client/src/pages/WatingGamePage.js
+++ client/src/pages/WatingGamePage.js
@@ -22,10 +22,11 @@ import { useContext } from "react";
 
 export default function WatingGamePage() {
   const { room_id } = useParams();
-  const { state } = useLocation();
+  const { state } = useLocation(); // 유저 목록 정보
 
   const [numberOfUser, setNumberOfUser] = useState(state.length);
   const [users, setUsers] = useState(state);
+  const [start, setStart] = useState(false); //방장이 게임시작했는지 여부
   const isAuthorized = getUserAffiliation() === "STAFF";
 
   const person_id = getUserId();
@@ -46,6 +47,13 @@ export default function WatingGamePage() {
           } else if (res.request === "disconnected") {
             setUsers(res.data);
           }
+          break;
+        case "room": //게임 시작 요청
+          setStart(true);
+
+          break;
+        case "hand_list": //해당 방의 손 목록 정보
+          navigate(`/room/${room_id}/game`, { state: res.data });
       }
     }
   }, [ready, send, res]); // 메시지가 도착하면
@@ -60,7 +68,17 @@ export default function WatingGamePage() {
     }
   };
 
-  const _startGame = () => {};
+  const _startGame = () => {
+    if (ready) {
+      let request = {
+        request: "start",
+        time_offset: 5, // seconds, 플레이 중인 방으로 전환 후 처음 손을 입력받기까지 기다리는 시간
+        time_duration: 60, // seconds, 처음 손을 입력받기 시작한 후 손을 입력받는 시간대의 길이
+      };
+      send(request);
+      navigate(`/room/${room_id}/game`);
+    }
+  };
   return (
     <Container>
       <Medium color="white" size={"60px"}>
