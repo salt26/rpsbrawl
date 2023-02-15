41ec5a03bf35a431dc7d009123dd71f016d9ed8d
flowersayo <flowersayo0811@ewhain.net>
2023-02-15 15:06:09 +0900

    add : add packages assets and styles
    font, icon images, color palette, packages

--- client/src/styles/font.js
+++ client/src/styles/font.js
@@ -4,4 +4,47 @@ export const Medium = styled.text`
   font-family: "KOTRAHOPE";
   font-size: ${({ size }) => (size ? size : "50px")};
   color: ${({ color }) => (color ? color : "black")};
+  text-shadow: ${({ shadow }) =>
+    shadow ? " 0px 5.17647px 5.17647px rgba(0, 0, 0, 0.25);" : "none"};
+`;
+
+export const GradientText = styled.text`
+  font-family: "KOTRAHOPE";
+  font-size: ${({ size }) => (size ? size : "50px")};
+  background: ${({ bg }) => (bg ? bg : "none")};
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+`;
+export const NavyGradient = styled.text`
+  font-family: "KOTRAHOPE";
+  font-size: ${({ size }) => (size ? size : "50px")};
+  color: ${({ color }) => (color ? color : "black")};
+  background: linear-gradient(180deg, #3ab6bc 0%, #3a66bc 100%, #2f508e 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+`;
+export const RedGradient = styled.text`
+  font-family: "KOTRAHOPE";
+  font-size: ${({ size }) => (size ? size : "50px")};
+  color: ${({ color }) => (color ? color : "black")};
+  background: linear-gradient(180deg, #3ab6bc 0%, #3a66bc 100%, #2f508e 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+`;
+
+export const MediumOutline = styled.text`
+  font-family: "KOTRAHOPE";
+  font-size: ${({ size }) => (size ? size : "50px")};
+  color: ${({ color }) => (color ? color : "black")};
+  -webkit-text-stroke: 1px white;
+  font-weight: 400;
+  line-height: 17px;
+  text-align: center;
+  flex-direction: row;
+  display: flex;
+
+  text-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
 `;
