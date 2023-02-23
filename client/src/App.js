b5d2e17b5f743b97f9d815761c0bfe99a61c8760
flowersayo <flowersayo0811@ewhain.net>
2023-02-23 12:34:01 +0900

    add: add music

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,8 @@ import { WebsocketProvider } from "./utils/WebSocketProvider";
 import { LanguageContext } from "./utils/LanguageProvider";
 import Toggle from "./components/Toggle";
 import { useLocation } from "react-router-dom";
-
+import WatingMusicSrc from "../src/assets/bgm/Melody_of_travel_fluttering_ver..mp3";
+import GameMusic from "../src/assets/bgm/Melody_of_tongtong.mp3";
 const Background = styled.div`
   width: 100%;
   height: 100vh;
@@ -27,16 +28,30 @@ function App() {
   //언어 모드
   const [mode, setMode] = useState(0);
   const { pathname } = useLocation();
+
   console.log(pathname);
+  const isInGame = () => {
+    if (pathname == "/" || pathname == "/rooms") return false;
+    else return true;
+  };
   return (
     <WebsocketProvider>
       <LanguageContext.Provider value={mode}>
         <Background>
           {pathname == "/" && <Toggle mode={mode} setMode={setMode} />}
 
+          {isInGame || (
+            <audio src={WatingMusicSrc} autoPlay={true} loop={true}></audio>
+          )}
+
+          {isInGame && (
+            <audio src={GameMusic} autoPlay={true} loop={true}></audio>
+          )}
+
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/rooms/:room_id/*" element={<GameRoomPage />} />
+
             <Route path="/rooms" element={<RoomListPage />} />
           </Routes>
         </Background>
