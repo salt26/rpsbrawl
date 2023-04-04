aef8578e7ec3285a08701a99c252353737e2264d
Dantae An <48adt@naver.com>
2023-04-04 04:48:56 +0000

    ignore Config.js

--- client/src/Config.js
+++
@@ -1,9 +0,0 @@
-export const BASE_SERVER_URL = "http://43.206.225.176:8000";
-export const BASE_WEBSOCKET_URL = "ws://43.206.225.176:8000";
-
-/*세팅 값*/
-export const COOL_TIME = 1;
-export const TIME_DURATION = 60; // 게임 시간
-export const TIME_OFFSET = 5; //
-export const RESULT_TIME = 20; // 결과창 보는시간
-export const PASSWORD = "unijam2023";
